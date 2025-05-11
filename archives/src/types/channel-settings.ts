b5c1ff9619a593df80663e5fa6d44d4d1c78946e
RedBoardDev <thomas.ott@epitech.eu>
2025-05-11 19:34:58 +0300

    chore: remove archived code

--- archives/src/types/channel-settings.ts
+++
@@ -1,7 +0,0 @@
-export interface ChannelSettings {
-  image: boolean;
-  tag: { type: 'user' | 'role'; id: string } | false;
-}
-
-export type GuildConfig = Record<string, ChannelSettings>;
-export type ConfigFile  = Record<string, GuildConfig>;
